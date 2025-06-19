2d185700be1b964c9ab269f7f82f1e0e865cf6e1
Sahil bhatt <sahilbhatt540@gmail.com>
2025-06-19 17:07:53 +0530

    Initial font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'sans-serif']
+         josefin: ['"Josefin Sans"', 'sans-serif'],
       }
     },
   },
